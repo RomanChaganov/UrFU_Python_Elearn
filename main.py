a5eff41a27eccfb1e059d3fd24a0c861d62ff2cd
RomanChaganov <romantchaganov@yandex.ru>
2022-11-23 20:16:10 +0500

    Made it possible to select the output

---
+++ main.py
@@ -0,0 +1,10 @@
+import report_out
+import table_out
+
+type_out = input('Введите данные для печати: ')
+if type_out == 'Вакансии':
+    table_out.InputConnect()
+elif type_out == 'Статистика':
+    report_out.InputConnect()
+else:
+    print('Некорректный ввод!')
