8ddc83f10a4eb797d6cd762d358bd4470a311819
romanchaganov <romantchaganov@yandex.ru>
2022-12-26 17:10:27 +0500

    Задание 3.5.1

--- task3.5.1.py
+++ task3.5.1.py
@@ -1,4 +1,8 @@
 import sqlite3
+import pandas as pd
 
+# df = pd.read_csv('Currency_data.csv')
 with sqlite3.connect('database.db') as db:
+    # df.to_sql(name='currency', con=db)
     cursor = db.cursor()
+    print(cursor.fetchone())
